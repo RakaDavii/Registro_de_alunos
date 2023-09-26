b44e4bff48dbc49d5792f4731a8a949bf65f13fa
Davi Felipe <128000829+RakaDavii@users.noreply.github.com>
2023-09-26 08:46:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const select = document.querySelector('[]')
+c
+// Gatilho de evento para, quando o "metodo" sofrer alguma alteração, a função exibirForm é acionada
+document.getElementById("metodo").addEventListener("change", exibirForm);
+
+// Função
+function exibirForm() {
+
+  // Atribuindo a variável para a seleção 
+  var elemento = document.getElementById("div1");
+
+  // Verificando a opção escolhida no select
+  if(document.getElementById("metodo").value == "incluir") {
+
+    // Alterando a visibilidade do elemento para "visível"
+    elemento.style.display = "block";
+  } else {
+
+    // Alterando a visibilidade do elemento para "invisível"
+    elemento.style.display = "none";
+  }
+
+
+}
